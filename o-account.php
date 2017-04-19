e58cc906e0c662353611e38ab714a2f79ca4ad99
Kalvin Manson <kalvinmanson@gmail.com>
2017-04-19 17:15:07 -0500

    account

---
+++ o-account.php
@@ -0,0 +1,107 @@
+<div class="inner innerbg1">
+    <div class="page">
+        <div class="bigline">
+            <div class="inside"><span>Mi cuenta</span></div>
+        </div>
+        <h4 class="right">
+            @nombre<br />
+            C.C. xxxxxxxxxxxxx Bofota D.C.
+            Cel. xxx xxxxxx<br />
+            Dir. cll xx # xx-xx
+        </h4>
+        
+        <h3 class="right">Historial de compra</h3>
+        <table class="table table-hover table-prin">
+            <tr>
+                <th>Acciones</th>
+                <th>Imagen</th>
+                <th>Nombre del producto</th>
+                <th>Unidad</th>
+                <th>Talla</th>
+                <th>Color</th>
+                <th>Descuento</th>
+                <th>Total</th>
+            </tr>
+            <tr>
+                <td><a href="#"><i class="fa fa-remove"></i></td>
+                <td><img src="assets/img/pro01.jpg" width="50"></td>
+                <td>FIST AID KIT WATERPROOF</td>
+                <td>1 UND.</td>
+                <td>N/A</td>
+                <td>Red</td>
+                <td>40%</td>
+                <td>$120.000</td>
+            </tr>
+            <tr>
+                <td><a href="#"><i class="fa fa-remove"></i></td>
+                <td><img src="assets/img/pro01.jpg" width="50"></td>
+                <td>FIST AID KIT WATERPROOF</td>
+                <td>1 UND.</td>
+                <td>N/A</td>
+                <td>Red</td>
+                <td>40%</td>
+                <td>$120.000</td>
+            </tr>
+            <tr>
+                <td><a href="#"><i class="fa fa-remove"></i></td>
+                <td><img src="assets/img/pro01.jpg" width="50"></td>
+                <td>FIST AID KIT WATERPROOF</td>
+                <td>1 UND.</td>
+                <td>N/A</td>
+                <td>Red</td>
+                <td>40%</td>
+                <td>$120.000</td>
+            </tr>
+        </table>
+        <p align="right">
+            <a href="#" class="btn btn-primary">Borrar Historial</a>
+        </p>
+        <h3 class="right">Lista de deseos</h3>
+        <table class="table table-hover table-prin">
+            <tr>
+                <th>Acciones</th>
+                <th>Imagen</th>
+                <th>Nombre del producto</th>
+                <th>Unidad</th>
+                <th>Talla</th>
+                <th>Color</th>
+                <th>Descuento</th>
+                <th>Total</th>
+            </tr>
+            <tr>
+                <td><a href="#"><i class="fa fa-remove"></i></td>
+                <td><img src="assets/img/pro01.jpg" width="50"></td>
+                <td>FIST AID KIT WATERPROOF</td>
+                <td>1 UND.</td>
+                <td>N/A</td>
+                <td>Red</td>
+                <td>40%</td>
+                <td>$120.000</td>
+            </tr>
+            <tr>
+                <td><a href="#"><i class="fa fa-remove"></i></td>
+                <td><img src="assets/img/pro01.jpg" width="50"></td>
+                <td>FIST AID KIT WATERPROOF</td>
+                <td>1 UND.</td>
+                <td>N/A</td>
+                <td>Red</td>
+                <td>40%</td>
+                <td>$120.000</td>
+            </tr>
+            <tr>
+                <td><a href="#"><i class="fa fa-remove"></i></td>
+                <td><img src="assets/img/pro01.jpg" width="50"></td>
+                <td>FIST AID KIT WATERPROOF</td>
+                <td>1 UND.</td>
+                <td>N/A</td>
+                <td>Red</td>
+                <td>40%</td>
+                <td>$120.000</td>
+            </tr>
+        </table>
+        <p align="right">
+            <a href="#" class="btn btn-primary">Borrar Lista</a>
+        </p>
+    </div>
+</div>
+
